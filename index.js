32ed9615098707ddb84420c7ca2375d896f4411c
JhonSmithRomeroSolorzano <thimsoremor@hotmail.com>
2022-08-05 16:25:22 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+const prompt = require("prompt-sync")({ sigint: true });
+const inputNumbers = prompt("Write the numbers list separated by a comma:");
+const inputNumberSum = prompt("Write the number sum value:");
+
+let numbersArray = inputNumbers.split(',')
+
+let filteredNumbers = numbersArray.filter(number => Number.isInteger(parseInt(number))).map(n=> parseInt(n));
+
+let hashObj = {}, results = [];
+
+for (let i = 0; i < filteredNumbers.length; i++){
+  if (hashObj[filteredNumbers[i]] || hashObj[filteredNumbers[i]] === 0){
+      results.push([hashObj[filteredNumbers[i]], filteredNumbers[i]])
+  }else{
+    hashObj[inputNumberSum - filteredNumbers[i]] = filteredNumbers[i];
+  }
+  
+}
